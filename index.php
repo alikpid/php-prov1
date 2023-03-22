472347654725e61f012044bd5cc18b52dc82e0d2
AlinaK <koromislo.molodec@gmail.com>
2023-03-23 02:56:19 +0700

    я сделал. но почему так быстро?

--- index.php
+++ index.php
@@ -1,15 +1,38 @@
 <?php
-$a = (int) decoct(15);
-echo $a;
-echo gettype($a);
+$xArray = [];
+for($i=0; $i<6; $i++) { $xArray[]=rand(1,100); }
 
-$xArray = [2, 4, 17, 47, 3];
-$yArray = [83, 61, 29, 30, 12];
-$zArray = [23, 62, 20, 83, 78];
+$yArray = [];
+for($i=0; $i<6; $i++) { $yArray[]=rand(1,100); }
+
+$zArray = [];
+for($i=0; $i<6; $i++) { $zArray[]=rand(1,100); }
+
 $uArray = [];
+$tempArray = array_merge($xArray, $yArray, $zArray);
 
-for ($i= 0; max(count($xArray) + count($yArray)); $i++) {
-//    if($xArray[$i] != null)
-        $uArray[] = $i;
+for ($i = 0; $i < count($tempArray); $i++) {
+    $uArray[$i] = (int) decoct($tempArray[$i]);
 }
+sort($uArray);
+
+//echo '<pre>';
+//print_r($xArray);
+//echo '</pre>';
+//
+//echo '<pre>';
+//print_r($yArray);
+//echo '</pre>';
+//
+//echo '<pre>';
+//print_r($zArray);
+//echo '</pre>';
+
+echo '<pre>';
 print_r($uArray);
+echo '</pre>';
+
+$uArray = array_slice($uArray, -5, 5);
+echo 'The largest numbers in the array: ' . implode(', ', $uArray);
+
+
